99b899097639ed8afc26469a2945dbee492020f0
Regine <regine.sioh.2021@smu.edu.sg>
2025-08-09 00:52:41 +0800

    use env var for frontend

--- frontend/app/src/constants.ts
+++ frontend/app/src/constants.ts
@@ -1,7 +1,7 @@
 export const SERVICE_URLS = {
-  USERS: "http://localhost:5001",
-  BOOKS: "http://localhost:5002",
-  ORDERS: "http://localhost:5003",
-  PLACE_ORDER: "http://localhost:5004",
-  DISPLAY_ORDERS: "http://localhost:5005",
+  USERS: import.meta.env.VITE_USERS_URL!,
+  BOOKS: import.meta.env.VITE_BOOKS_URL!,
+  ORDERS: import.meta.env.VITE_ORDERS_URL!,
+  PLACE_ORDER: import.meta.env.VITE_PLACE_ORDER_URL!,
+  DISPLAY_ORDERS: import.meta.env.VITE_DISPLAY_ORDERS_URL!,
 };
